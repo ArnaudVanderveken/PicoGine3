84ddd8008685b43b60425df446545f575d64e953
Arnaud Vanderveken <vanderveken.arnaud@gmail.com>
2024-12-03 20:45:39 +0100

    SetProcessDpiAwarenessContext instead of SetThreadDpiAwarenessContext

--- PicoGine3/main.cpp
+++ PicoGine3/main.cpp
@@ -7,14 +7,14 @@ int APIENTRY wWinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPWS
 	// Check for DirectX Math library support.
 	if (!XMVerifyCPUSupport())
 	{
-		MessageBoxA(nullptr, "Failed to verify DirectX Math library support.", "Error", MB_OK | MB_ICONERROR);
+		::MessageBoxA(nullptr, "Failed to verify DirectX Math library support.", "Error", MB_OK | MB_ICONERROR);
 		return 1;
 	}
 
 	// Windows 10 Creators update adds Per Monitor V2 DPI awareness context.
 	// Using this awareness context allows the client area of the window to achieve 100% scaling
 	// while still allowing non-client window content to be rendered in a DPI sensitive fashion.
-	SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+	::SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
 
 	const auto& core = CoreSystems::Get();
 

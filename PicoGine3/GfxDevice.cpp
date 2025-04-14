2e710889954b16f030f75b6d817072f1a72a0e76
Arnaud Vanderveken <vanderveken.arnaud@gmail.com>
2025-04-14 12:11:53 +0200

    vk 1.3 api

--- PicoGine3/GfxDevice.cpp
+++ PicoGine3/GfxDevice.cpp
@@ -336,7 +336,7 @@ void GfxDevice::CreateVkInstance()
 	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
 	appInfo.pEngineName = "PicoGine3";
 	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-	appInfo.apiVersion = VK_API_VERSION_1_0;
+	appInfo.apiVersion = VK_API_VERSION_1_3;
 
 	VkInstanceCreateInfo createInfo{};
 	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
